26723fb2498c3ae393a9737d01c8250befb6b416
jeremytzh <jeremytanstone@gmail.com>
2025-04-13 12:23:08 +0800

    added lifetime limit to the area effector implementation via max_hp

--- Scenes/entities/effectors/static_area.gd
+++ Scenes/entities/effectors/static_area.gd
@@ -1,9 +1,13 @@
 extends "res://Scenes/entities/effectors/effector_entity.gd"
 
-
+func _process(delta: float):
+	stats.take_stat_damage(StatModification.new({ATTRIBUTE_ENUMS.TYPE.MAX_HP: -1}))
+	#MAX_HP here counts the lifetime of this object in process frames 
+	if stats.get_stat(ATTRIBUTE_ENUMS.TYPE.MAX_HP) <= 0:
+		queue_free()
 
 func child_ready():
-	print("circular effector init")
+	#print("circular effector init")
 	var collision_shape = $hurtbox/CollisionShape2D
 	var circle_shape2d = CircleShape2D.new()
 	circle_shape2d.set_radius(stats.get_stat(ATTRIBUTE_ENUMS.TYPE.MAX_MP))
